f175cc5fbe91330630030f5e37c671f3a038631c
Vikash Chauhan <vikashchauhan51@gmail.com>
2020-05-10 12:21:51 +0530

    inital setup

---
+++ AgeCal/AgeCal/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AgeCal.Models;
+
+namespace AgeCal.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
