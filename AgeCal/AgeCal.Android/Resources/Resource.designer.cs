425f76705002a4ac360343cf10ceb51a498ce736
Vikash Chauhan <vikashchauhan51@gmail.com>
2020-05-30 21:09:14 +0530

    splash implemented

--- AgeCal/AgeCal.Android/Resources/Resource.designer.cs
+++ AgeCal/AgeCal.Android/Resources/Resource.designer.cs
@@ -2346,26 +2346,26 @@ namespace AgeCal.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02015f
-			public const int avd_hide_password_1 = 2130837855;
-			
 			// aapt resource value: 0x7f020160
-			public const int avd_hide_password_2 = 2130837856;
+			public const int avd_hide_password_1 = 2130837856;
 			
 			// aapt resource value: 0x7f020161
-			public const int avd_hide_password_3 = 2130837857;
+			public const int avd_hide_password_2 = 2130837857;
+			
+			// aapt resource value: 0x7f020162
+			public const int avd_hide_password_3 = 2130837858;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020162
-			public const int avd_show_password_1 = 2130837858;
-			
 			// aapt resource value: 0x7f020163
-			public const int avd_show_password_2 = 2130837859;
+			public const int avd_show_password_1 = 2130837859;
 			
 			// aapt resource value: 0x7f020164
-			public const int avd_show_password_3 = 2130837860;
+			public const int avd_show_password_2 = 2130837860;
+			
+			// aapt resource value: 0x7f020165
+			public const int avd_show_password_3 = 2130837861;
 			
 			// aapt resource value: 0x7f020056
 			public const int back = 2130837590;
@@ -3078,11 +3078,11 @@ namespace AgeCal.Droid
 			// aapt resource value: 0x7f020142
 			public const int notification_icon_background = 2130837826;
 			
-			// aapt resource value: 0x7f02015d
-			public const int notification_template_icon_bg = 2130837853;
-			
 			// aapt resource value: 0x7f02015e
-			public const int notification_template_icon_low_bg = 2130837854;
+			public const int notification_template_icon_bg = 2130837854;
+			
+			// aapt resource value: 0x7f02015f
+			public const int notification_template_icon_low_bg = 2130837855;
 			
 			// aapt resource value: 0x7f020143
 			public const int notification_tile_bg = 2130837827;
@@ -3127,40 +3127,43 @@ namespace AgeCal.Droid
 			public const int share_black_white = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int team_black = 2130837841;
+			public const int splash_screen = 2130837841;
 			
 			// aapt resource value: 0x7f020152
-			public const int teams = 2130837842;
+			public const int team_black = 2130837842;
 			
 			// aapt resource value: 0x7f020153
-			public const int time_black = 2130837843;
+			public const int teams = 2130837843;
 			
 			// aapt resource value: 0x7f020154
-			public const int tools = 2130837844;
+			public const int time_black = 2130837844;
 			
 			// aapt resource value: 0x7f020155
-			public const int tooltip_frame_dark = 2130837845;
+			public const int tools = 2130837845;
 			
 			// aapt resource value: 0x7f020156
-			public const int tooltip_frame_light = 2130837846;
+			public const int tooltip_frame_dark = 2130837846;
 			
 			// aapt resource value: 0x7f020157
-			public const int trash = 2130837847;
+			public const int tooltip_frame_light = 2130837847;
 			
 			// aapt resource value: 0x7f020158
-			public const int user_info = 2130837848;
+			public const int trash = 2130837848;
 			
 			// aapt resource value: 0x7f020159
-			public const int watch = 2130837849;
+			public const int user_info = 2130837849;
 			
 			// aapt resource value: 0x7f02015a
-			public const int wifi = 2130837850;
+			public const int watch = 2130837850;
 			
 			// aapt resource value: 0x7f02015b
-			public const int wifioff_black = 2130837851;
+			public const int wifi = 2130837851;
 			
 			// aapt resource value: 0x7f02015c
-			public const int wifion_black = 2130837852;
+			public const int wifioff_black = 2130837852;
+			
+			// aapt resource value: 0x7f02015d
+			public const int wifion_black = 2130837853;
 			
 			static Drawable()
 			{
